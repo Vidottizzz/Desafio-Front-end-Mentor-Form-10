108bf7c4742acb1cd2ec909865c779d6960d31a1
Vidottizzz <vidottizzz@gmail.com>
2021-12-30 17:18:34 -0300

    desafio finalizado

---
+++ script.js
@@ -0,0 +1,26 @@
+const pinkBtn = document.querySelector('.pink-button');
+const emailInpt = document.querySelector('#email');
+const footer = document.querySelector('.footer');
+const errImg = document.querySelector('.error-image');
+const result = document.querySelector('.result-p');
+
+
+pinkBtn.addEventListener('click', function(){
+
+    if(emailInpt.value == "" || emailInpt.value.indexOf('@') == -1 || emailInpt.value.indexOf('.') == -1){
+     
+        errImg.classList.remove('hide');
+        errImg.classList.add('show');
+        emailInpt.classList.add('border-red');
+        result.innerHTML = 'Please provide a valid email';
+        emailInpt.focus();
+    } else {
+        emailInpt.value = " ";
+        errImg.classList.add('hide');
+        errImg.classList.remove('show');
+        emailInpt.classList.remove('border-red');
+        result.innerHTML = " ";
+    }
+      
+})
+
